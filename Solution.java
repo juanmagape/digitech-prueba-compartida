532574de9bfb13035325c65068d3b770e8b5ad94
Juan Manuel Galán Pérez <juanmagape.trb@gmail.com>
2025-10-01 10:08:58 +0200

    Añadir código

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static void main(String[] args) {
+
+        Scanner print = new Scanner(System.in);
+
+        float gradosC = print.nextInt();
+
+        float fahrenheit = gradosC * 9/5 + 32;
+        float kelvin = gradosC + 273.15f;
+
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+        System.out.println("Kelvin: " + kelvin);
+
+    }
+}
